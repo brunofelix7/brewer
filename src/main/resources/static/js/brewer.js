d2d13dada5dbd8754415e8457d8ec4f00d8d883d
Bruno Felix <indra.bruno@compagas.local>
2020-10-13 12:33:07 -0300

    Conclusao da aula 5.8. Organizando o layout com fragmentos

--- src/main/resources/static/js/brewer.js
+++ src/main/resources/static/js/brewer.js
@@ -1,7 +1,9 @@
 $(function() {
+	//	Adiciona máscara de formatação para campos com casas decimais
 	var decimal = $('.js-decimal');
 	decimal.maskMoney();
 	
+	//	Adiciona máscara de formatação para campos inteiros
 	var whole = $('.js-whole');
 	whole.maskMoney({ precision: 0 });
 });
